850707252a9e261703153da1f66df0c95cabcbd3
MUBEENAUQI <49668351+MUBEENAUQI@users.noreply.github.com>
2023-07-04 12:27:58 +0530

    Admin section completed

---
+++ Models/Salesman_Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinic_Automation.Models
+{
+    public class Salesman_Model
+    {
+        public int? Salesman_id { get; set; }
+        public int? Login_id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Salesman's name.")]
+        public string Salesman_name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter the Salesman's phone number.")]
+        public string Salesman_Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Salesman's email.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Salesman_Email { get; set; }
+
+        [Required(ErrorMessage = "Please select the Salesman's Gender")]
+
+        public string Salesman_Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Salesman's username.")]
+        public string Salesman_Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Salesman's password.")]
+        public string Salesman_Password { get; set; }
+    }
+}
---
+++ Models/Supplier_Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinic_Automation.Models
+{
+    public class Supplier_Model
+    {
+        public int? Supplier_id { get; set; }
+        public int? Login_id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Supplier's name.")]
+        public string Supplier_name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter the Supplier's phone number.")]
+        public string Supplier_Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Supplier's email.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Supplier_Email { get; set; }
+
+        [Required(ErrorMessage = "Please select the Supplier's Gender")]
+
+        public string Supplier_Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Supplier's username.")]
+        public string Supplier_Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Supplier's password.")]
+        public string Supplier_Password { get; set; }
+    }
+}
